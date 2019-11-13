2fe708a0289b17166696f338c994dd4565bab4b0
Serguchev Sergey <s.serge@bk.ru>
2019-11-13 23:42:55 +0300

    http -> https

--- wcs-webclient/src/environments/environment.prod.ts
+++ wcs-webclient/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseHref: '/',
-  apiUrl: 'http://178.62.212.25:8080/api/',
+  apiUrl: 'https://mebel-40.ru:8443/api/',
   noImageUrl: 'assets/img/no-image.png'
 };
